ea1e98019dfc887ba4064d1637b1b2770672252d
Enrico Stano <ocirne.onats@gmail.com>
2017-08-10 14:55:27 +0000

    :rocket:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyStore::Application.config.secret_token = '831b29a3a6b18d5b62e63aba8983eca7f2cefdc7b55436daa6697bdf74696368ed57307744e78bf0b727b84795f470212bc7565fc8ba627f7d2c8bcecaed9abd'
